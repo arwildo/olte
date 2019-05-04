f55431220857b4f876559d659846178a2704eef2
Arwildo <arwildo@gmail.com>
2019-05-04 22:38:11 +0800

    Minor fix

--- main/views.py
+++ main/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import ToDoList, Item
 from .forms import CreateNewList
-# Create your views here.
 
+# Create your views here.
 def index(response, id):
 	ls = ToDoList.objects.get(id=id)
 	return render(response, 'main/list.html', {'ls':ls})
