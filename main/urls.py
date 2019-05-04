c87787dd2be7224de0ab9a4ad4f10a3ea8cd7c54
Arwildo <arwildo@gmail.com>
2019-05-04 17:11:34 +0800

    Add forms

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
 path('<int:id>', views.index, name='index'),
-path('', views.home, name='home')
+path('', views.home, name='home'),
+path('create/', views.create, name='create'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import ToDoList, Item
-
+from .forms import CreateNewList
 # Create your views here.
 
 def index(response, id):
@@ -11,3 +11,17 @@ def index(response, id):
 def home(response):
 	return render(response, 'main/home.html', {})
 
+def create(response):
+	if response.method == "POST":
+		form = CreateNewList(response.POST)
+
+		if form.is_valid():
+			n = form.cleaned_data["name"]
+			t = ToDoList(name=n)
+			t.save()
+
+		return HttpResponseRedirect("/%i" %t.id)
+
+	else:
+		form = CreateNewList()
+	return render(response, 'main/create.html', {"form":form})
