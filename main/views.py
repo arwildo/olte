1b8412cd69ffb70274700a15288693addcd4017f
Arwildo <arwildo@gmail.com>
2019-05-05 15:47:47 +0800

    Add custom forms

--- main/views.py
+++ main/views.py
@@ -6,6 +6,25 @@ from .forms import CreateNewList
 # Create your views here.
 def index(response, id):
 	ls = ToDoList.objects.get(id=id)
+
+	if response.method == "POST":
+		print(response.POST)
+		if response.POST.get("save"):
+			for item in ls.item_set.all():
+				if response.POST.get("c" + str(item.id)) == "clicked":
+					item.complete = True
+				else:
+					item.complete = False
+				item.save()
+
+		elif response.POST.get("newItem"):
+			txt = response.POST.get("new")
+
+			if len(txt) > 2:
+				ls.item_set.create(text=txt, complete=False)
+			else:
+				print("Invalid Input")
+				
 	return render(response, 'main/list.html', {'ls':ls})
 
 def home(response):
