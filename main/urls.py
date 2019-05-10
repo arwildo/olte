0bdece6a944296903e562091e330714a4f5cd171
Arwildo <arwildo@gmail.com>
2019-05-10 23:18:44 +0800

    Add static files

--- main/urls.py
+++ main/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path('<int:id>', views.index, name='index'),
-path('', views.home, name='home'),
-path('create/', views.create, name='create'),
-path('view/', views.view, name='view'),
+    path('<int:id>', views.index, name='index'),
+    path('', views.home, name='home'),
+    path('create/', views.create, name='create'),
+    path('view/', views.view, name='view'),
 ]
--- mysite/urls.py
+++ mysite/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from register import views as v
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,3 +24,5 @@ urlpatterns = [
     path('', include('main.urls')),
     path('', include("django.contrib.auth.urls")),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
